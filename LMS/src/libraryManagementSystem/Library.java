8364fc80281765804ae83d5e61fa9d2892f2e050
mertbahadir <mert-bahadir@hotmail.com>
2016-11-03 15:37:59 +0300

    simple search method

--- LMS/src/libraryManagementSystem/Library.java
+++ LMS/src/libraryManagementSystem/Library.java
@@ -1,6 +1,31 @@
 package libraryManagementSystem;
 
-public class Library {
-	String a;
+import java.util.ArrayList;
 
+import libraryManagementSystem.Book;
+import libraryManagementSystem.DataImporter;
+
+
+
+public class Library
+{
+
+    public ArrayList<Book> search(String searchedString)
+    {
+        if (searchedString.isEmpty())
+        {
+            return null;
+        }
+        ArrayList<Book> bookList = new ArrayList<>(DataImporter.creatingData()); //booklist library e eklendiği zaman kaldırılacak library'nin booklisti kullanılacak.
+        ArrayList<Book> resultBookList = new ArrayList<>();
+
+        for (int i = 0; i < bookList.size(); i++)
+        {
+            if (bookList.get(i).getTitle().toLowerCase().contains(searchedString.toLowerCase()))
+            {
+                resultBookList.add(bookList.get(i));
+            }
+        }
+        return resultBookList;
+    }
 }
