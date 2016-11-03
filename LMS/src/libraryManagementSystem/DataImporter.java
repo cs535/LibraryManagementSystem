48d09e94bd7a3cad01406c84cb64edc45d0bbc8f
gokhangu <gokhangu@gokhangu-ThinkPad-T540p>
2016-11-03 06:51:00 +0200

    dataImporter

---
+++ LMS/src/libraryManagementSystem/DataImporter.java
@@ -0,0 +1,70 @@
+package libraryManagementSystem;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import libraryManagementSystem.Book;
+
+public class DataImporter {
+
+	public static ArrayList<Book> creatingData() {
+		
+		String fileName = "./data/Books.csv";
+		
+		File fileFromDataReading = new File(fileName);
+		
+		FileReader fr = null;
+		
+		if(!fileFromDataReading.exists()){
+			
+			System.out.println("FAILED: " + fileName + " FILE does not exist");
+			return null;	
+		}
+		
+		try {		
+			fr = new FileReader(fileFromDataReading);	
+		} 
+		
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		BufferedReader br = new BufferedReader(fr);
+			 
+		ArrayList<Book> bookList = new ArrayList<>();
+		
+ 	    try {
+ 	    	String str = null;
+		    while((str = br.readLine()) != null)
+		    {	
+			    Book b = new Book();
+			    String[] arr = str.split(";");
+			    b.setIsbn(Integer.valueOf(arr[0]));
+			    b.setTitle(String.valueOf(arr[1]));
+			    b.setAuthor(String.valueOf(arr[2]));
+			    b.setYear(Integer.valueOf(arr[3]));
+			    b.setPublishing_house(String.valueOf(arr[4]));
+			    b.setImage(String.valueOf(arr[5]));
+			    bookList.add(b);
+	    	}
+	    } 	
+    	catch (IOException e) {
+	    	e.printStackTrace();
+	    }
+
+		
+		 try {
+		     br.close();
+			 fr.close();
+		 } 
+		 
+		 catch (IOException e) {
+			e.printStackTrace();
+		 }
+		 
+		return bookList;
+	}
+}
