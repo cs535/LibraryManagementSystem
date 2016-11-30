b20da25b3ad250a6cc93dd1528a59b064b1e696e
Songul Abuzar <songul.abuzar@gmail.com>
2016-11-30 10:43:23 +0200

    Fixed closing all pages error when book detail page is closed

--- LMS/src/view/BookDetail.java
+++ LMS/src/view/BookDetail.java
@@ -49,7 +49,7 @@ public class BookDetail extends JFrame {
 		this.book = book;
 		
 		setTitle("Book Detail");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 701, 412);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
