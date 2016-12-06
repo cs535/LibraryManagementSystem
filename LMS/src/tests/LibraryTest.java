ad0a893409949ed1592651ae98880ef4edbea0c6
Enes <enesgulgonul@gmail.com>
2016-12-06 12:31:14 +0400

    improved libraryTest by adding new test cases.

--- LMS/src/tests/LibraryTest.java
+++ LMS/src/tests/LibraryTest.java
@@ -19,6 +19,7 @@ public class LibraryTest extends TestCase {
 		ArrayList<Book> bookList = new ArrayList<Book>();
 		bookList.add(new Book("isbn1", "title1", "author1", "1", "house1", ""));
 		bookList.add(new Book("isbn2", "title1", "author2", "2", "house3", ""));
+		bookList.add(new Book("isbn2", "title1", "author3", "2", "house1", ""));
 		bookList.add(new Book("isbn3", "title2", "author1", "1", "house1", ""));
 		bookList.add(new Book("isbn4", "title3", "author1", "3", "house2", ""));
 		bookList.add(new Book("isbn5", "title3", "author3", "3", "house4", ""));
@@ -27,7 +28,7 @@ public class LibraryTest extends TestCase {
 	
 	public void testTitleSearch(){
 		ArrayList<Book> resultList = library.advancedSearch("title1", "", "", "", "");
-		assertEquals(resultList.size(), 2);
+		assertEquals(resultList.size(), 3);
 		for ( Book result : resultList)
 		{
 			assertEquals(result.getTitle(), "title1");
@@ -68,15 +69,142 @@ public class LibraryTest extends TestCase {
 		resultList = library.advancedSearch("", "", "", "house5", "");
 		assertEquals(resultList.size(), 0);
 	}
-	public void testMultiParameterSearch(){
+	
+	public void testTitleAndAuthorSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("title1", "author1", "", "", "");
+		assertEquals(resultList.size(), 1);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getTitle(), "title1");
+			assertEquals(result.getAuthor(), "author1");
+		}
+		resultList = library.advancedSearch("title2", "author2", "", "", "");
+		assertEquals(resultList.size(), 0);
+	}
+	
+	public void testTitleAndYearSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("title1", "", "2", "", "");
+		assertEquals(resultList.size(), 2);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getTitle(), "title1");
+			assertEquals(result.getYear(), "2");
+		}
+		resultList = library.advancedSearch("title1", "", "3", "", "");
+		assertEquals(resultList.size(), 0);
+	}
+	
+	public void testTitleAndHouseSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("title1", "", "", "house1", "");
+		assertEquals(resultList.size(), 2);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getTitle(), "title1");
+			assertEquals(result.getPublishing_house(), "house1");
+		}
+		resultList = library.advancedSearch("title1", "", "", "house2", "");
+		assertEquals(resultList.size(), 0);
+	}
+	
+	public void testAuthorAndYearSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("", "author1", "1", "", "");
+		assertEquals(resultList.size(), 2);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getAuthor(), "author1");
+			assertEquals(result.getYear(), "1");
+		}
+		resultList = library.advancedSearch("", "author1", "2", "", "");
+		assertEquals(resultList.size(), 0);
+		}
+	
+	public void testAuthorAndHouseSearch(){
 		ArrayList<Book> resultList = library.advancedSearch("", "author1", "", "house1", "");
 		assertEquals(resultList.size(), 2);
 		for ( Book result : resultList)
 		{
+			assertEquals(result.getAuthor(), "author1");
+			assertEquals(result.getPublishing_house(), "house1");
+		}
+		resultList = library.advancedSearch("", "author1", "", "house3", "");
+		assertEquals(resultList.size(), 0);
+		}
+	
+	public void testYearAndHouseSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("", "", "1", "house1", "");
+		assertEquals(resultList.size(), 2);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getYear(), "1");
+			assertEquals(result.getPublishing_house(), "house1");
+		}
+		resultList = library.advancedSearch("", "", "1", "house3", "");
+		assertEquals(resultList.size(), 0);
+	}
+	
+	public void testTitleAndAuthorAndYearSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("title1", "author1", "1", "", "");
+		assertEquals(resultList.size(), 1);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getTitle(), "title1");
+			assertEquals(result.getAuthor(), "author1");
+			assertEquals(result.getYear(), "1");
+		}
+		resultList = library.advancedSearch("title1", "author1", "2", "", "");
+		assertEquals(resultList.size(), 0);
+	}
+	
+	public void testTitleAndAuthorAndHouseSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("title1", "author1", "", "house1", "");
+		assertEquals(resultList.size(), 1);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getTitle(), "title1");
+			assertEquals(result.getAuthor(), "author1");
+			assertEquals(result.getPublishing_house(), "house1");
+		}
+		resultList = library.advancedSearch("title1", "author1", "", "house2", "");
+		assertEquals(resultList.size(), 0);
+	}
+	
+	public void testTitleAndYearAndHouseSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("title1", "", "1", "house1", "");
+		assertEquals(resultList.size(), 1);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getTitle(), "title1");
+			assertEquals(result.getPublishing_house(), "house1");
+			assertEquals(result.getYear(), "1");
+		}
+		resultList = library.advancedSearch("title1", "author1", "", "house2", "");
+		assertEquals(resultList.size(), 0);
+	}
+	
+	public void testAuthorAndYearAndHouseSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("", "author1", "1", "house1", "");
+		assertEquals(resultList.size(), 2);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getAuthor(), "author1");
+			assertEquals(result.getYear(), "1");
+			assertEquals(result.getPublishing_house(), "house1");
+		}
+		resultList = library.advancedSearch("", "author1", "2", "house1", "");
+		assertEquals(resultList.size(), 0);
+		}
+	
+	public void testAllParameterSearch(){
+		ArrayList<Book> resultList = library.advancedSearch("title1", "author1", "1", "house1", "");
+		assertEquals(resultList.size(), 1);
+		for ( Book result : resultList)
+		{
+			assertEquals(result.getTitle(), "title1");
 			assertEquals(result.getPublishing_house(), "house1");
 			assertEquals(result.getAuthor(), "author1");
+			assertEquals(result.getYear(), "1");
 		}
-		resultList = library.advancedSearch("", "author2", "", "house1", "");
+		resultList = library.advancedSearch("title1", "author2", "1", "house1", "");
 		assertEquals(resultList.size(), 0);
 	}
 
