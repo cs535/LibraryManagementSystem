bad60b79f57f6024773b6ccff8bc6fd1fb1bdf65
gokhangu <gokhangu@gokhangu-ThinkPad-T540p>
2016-12-07 08:50:58 +0200

    write data to a newly created file

--- LMS/src/libraryManagementSystem/DataExporter.java
+++ LMS/src/libraryManagementSystem/DataExporter.java
@@ -4,10 +4,11 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class DataExporter {
-	private static String writeDataToFile(String fileName, String path, StringBuilder sb) {
-		
+	public static String writeDataToFile(String fileName, String path, ArrayList<Book> resultBookList) {
+
 		// formatted data file
 		File fileForDataWriting = new File(path + fileName);
 		FileWriter fw;
@@ -21,7 +22,7 @@ public class DataExporter {
 			
 			fw = new FileWriter(fileForDataWriting.getAbsoluteFile());
 			bw = new BufferedWriter(fw);
-			bw.write(sb.toString());
+			bw.write(resultBookList.toString());
 			
 		} catch (IOException e1) {
 			
