e03424cd81a429f44659b5bc5d6f347d8d8f2935
gokhangu <gokhangu@gokhangu-ThinkPad-T540p>
2016-12-08 08:29:37 +0200

    DAta Importer Unit Test

--- LMS/src/tests/DataImporterTest.java
+++ LMS/src/tests/DataImporterTest.java
@@ -1,8 +1,11 @@
 package tests;
 
+import libraryManagementSystem.Book;
 import libraryManagementSystem.DataImporter;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -10,5 +13,15 @@ import junit.framework.TestCase;
 
 public class DataImporterTest extends TestCase {
 	
-
+	public void testCreatingData(){
+		
+		ArrayList<Book> resultList = DataImporter.creatingData("Books3.csv");
+		assertEquals(resultList.size(), 10);
+		
+		ArrayList<Book> resultList2 = DataImporter.creatingData("Books2.csv");
+		assertEquals(resultList2.size(), 100);
+		
+		ArrayList<Book> resultList3 = DataImporter.creatingData("BXBooks.csv");
+		assertEquals(resultList3.size(), 171294);
+	}
 }
